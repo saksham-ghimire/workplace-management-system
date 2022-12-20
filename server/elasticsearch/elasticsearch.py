c852ed067ec774ad0e7d0f83f8f3cd4e0e03041a
saksham-ghhimire <gsakshyam3@gmail.com>
2022-12-20 14:45:10 +0545

    added app activity and network graph

--- server/elasticsearch/elasticsearch.py
+++ server/elasticsearch/elasticsearch.py
@@ -10,21 +10,21 @@ import json
 
 # singleton design pattern
 class Elasticsearch:
-    
+
     __instance = None
+
     def __init__(self) -> None:
         if Elasticsearch.__instance != None:
             pass
             # log here
         else:
             self.__generic_query_parameters = {
-            "headers":{"content-type":"application/json"},
-            "auth":HTTPBasicAuth('elastic','password'),
-            # remove verify parameter if you don't have ssl configured on elasticsearch
-            "verify":'C:/Users/Saksham/Desktop/Pan-Ems/certs/ca/ca.crt',
-        }
+                "headers": {"content-type": "application/json"},
+                "auth": HTTPBasicAuth('elastic', 'password'),
+                # remove verify parameter if you don't have ssl configured on elasticsearch
+                "verify": 'C:/Users/Saksham/Desktop/Pan-Ems/certs/ca/ca.crt',
+            }
         Elasticsearch.__instance = self
-    
 
     @staticmethod
     def getInstance():
@@ -32,76 +32,104 @@ class Elasticsearch:
             Elasticsearch()
         return Elasticsearch.__instance
 
-    def __get(self,query) -> list:
+    def __get(self, query) -> list:
         try:
-            response = requests.get(**query,**self.__generic_query_parameters,timeout=8)
+            response = requests.get(
+                **query, **self.__generic_query_parameters, timeout=8)
             if response.status_code != 200:
                 log.debug(f'Elasticsearch responded with {response.content}')
                 return None
             resp = json.loads(response.content)
-            if resp.get("aggregations") == None: return [i.get("_source") for i in resp.get("hits").get("hits")]
-            return resp.get("aggregations").get("categories_agg").get("buckets")
-           
-        
-        except (ConnectionError,Timeout) as e:
-            log.debug(f'Unable to perform get opertation. Received exception : {e}')
+            if resp.get("aggregations") == None:
+                return [i.get("_source") for i in resp.get("hits").get("hits")]
+            return resp.get("aggregations")
+        except (ConnectionError, Timeout) as e:
+            log.debug(
+                f'Unable to perform get opertation. Received exception : {e}')
             return None
 
-    def __post(self,query):
+    def __post(self, query):
         try:
-            response = requests.post(**query,**self.__generic_query_parameters,timeout=8)
+            response = requests.post(
+                **query, **self.__generic_query_parameters, timeout=8)
             if response.status_code != 201:
-                log.debug(f'Elasticsearch responded with {response.content}{response.status_code}')
+                log.debug(
+                    f'Elasticsearch responded with {response.content}{response.status_code}')
                 return None
             return True
-        except (ConnectionError,Timeout) as e:
-            log.debug(f'Unable to perform post operation. Received exception : {e}')
+        except (ConnectionError, Timeout) as e:
+            log.debug(
+                f'Unable to perform post operation. Received exception : {e}')
             return None
-    
-    def registerNewWorkStation(self,data:dict) -> bool:
-        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostWorkstationInfo,params={"data":data}))
 
-    def getWorkstationInfo(self, hostname:str) -> dict:
-        response = self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetWorkstationInfo,params={"hostname":hostname}))
-        if response != None or len(response)!=0: return response[0] 
+    def registerNewWorkStation(self, data: dict) -> bool:
+        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostWorkstationInfo, params={"data": data}))
+
+    def getWorkstationInfo(self, hostname: str) -> dict:
+        response = self.__get(query=QuerySerializer.getDesignatedQuery(
+            reqType=SupportedQueries.GetWorkstationInfo, params={"hostname": hostname}))
+        if response != None or len(response) != 0:
+            return response[0]
         return None
-    
+
     def getRegisteredWorkstations(self) -> 'list[dict]':
-        response = self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetRegisteredWorkstations,params={}))
-        if response != None: return response
+        response = self.__get(query=QuerySerializer.getDesignatedQuery(
+            reqType=SupportedQueries.GetRegisteredWorkstations, params={}))
+        if response != None:
+            return response
         return None
 
-    def getNetworkLog(self,from_time:int,to_time:int,hostname:str=None):
-        return self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetNetworkLog,params={"from_time":from_time,"to_time":to_time,"hostname":hostname}))
+    def getNetworkLog(self, from_time: int, to_time: int, hostname: str = None):
+        return self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetNetworkLog, params={"from_time": from_time, "to_time": to_time, "hostname": hostname}))
 
+    def getNetworkUsage(self, from_time: int, to_time: int):
+        response = self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetNetworkUsage, params={
+                              "from_time": from_time, "to_time": to_time}))
+        if response == None:
+            return None
+        return response.get("categories_agg").get("buckets")
+        # response = {"InputBytes": abs(float(
+        #     response.get("input_bytes_sum_agg").get("value")))/1000,
+        #     "OutputBytes": float(response.get("output_bytes_sum_agg").get("value"))/1000,
+        #     "InputPackets": float(response.get("input_packets_sum_agg").get("value")),
+        #     "OutputPackets": float(response.get("output_packets_sum_agg").get("value"))
+        # }
+        return response
 
-    def getRuntimeProcessLog(self,from_time:int,to_time:int,hostname:str=None):
-        return self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetRuntimeProcessLog,params={"from_time":from_time,"to_time":to_time,"hostname":hostname}))
+    def getRuntimeProcessLog(self, from_time: int, to_time: int, hostname: str = None):
+        response = self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetRuntimeProcessLog, params={
+                              "from_time": from_time, "to_time": to_time, "hostname": hostname}))
+        if response == None:
+            return None
+        return response.get("categories_agg").get("buckets")
 
-    def getMonitoringProcessLog(self,from_time:int,to_time:int,hostname:str=None,process:str=None):
-        return self.__get(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.GetMonitoringProcessLog,params={"from_time":from_time,"to_time":to_time,"hostname":hostname,"process":process}))
+    def getUserActivityLog(self, from_time: int, to_time: int):
+        response = self.__get(query=QuerySerializer.getDesignatedQuery(
+            reqType=SupportedQueries.GetUserActivityLog, params={"from_time": from_time, "to_time": to_time}))
+        if response == None:
+            return None
+        return response.get("categories_agg").get("buckets")
 
+    def pushNetworkLog(self, hostname: str, data: structure_pb2.NetInformation):
+        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostNetworkInfo, params={"data": data, "hostname": hostname}))
 
-    def pushNetworkLog(self, hostname:str, data:structure_pb2.NetInformation):
-        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostNetworkInfo,params={"data":data,"hostname":hostname}))
+    def pushHealthLog(self, hostname: str, data: structure_pb2.SystemHealth):
+        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostSystemHealth, params={"data": data, "hostname": hostname}))
 
-    def pushHealthLog(self,hostname:str,data:structure_pb2.SystemHealth):
-        return self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostSystemHealth,params={"data":data,"hostname":hostname}))
-
-    def pushProcessRuntimeLog(self,hostname:str,data:'list[str]'):
+    def pushProcessRuntimeLog(self, hostname: str, data: 'list[str]'):
         for eachLog in data:
-            self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostRunningProcesses,params={"data":{"processName":eachLog},"hostname":hostname}))
+            self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostRunningProcesses, params={
+                        "data": {"processName": eachLog}, "hostname": hostname}))
 
-    def pushMonitoringProcessLog(self, hostname:str,data:dict):
+    def pushMonitoringProcessLog(self, hostname: str, data: dict):
         for eachLog in data:
-            self.__post(query=QuerySerializer.getDesignatedQuery(reqType=SupportedQueries.PostMonitoringProcessDetail,params={"data":eachLog,"hostname":hostname}))
+            self.__post(query=QuerySerializer.getDesignatedQuery(
+                reqType=SupportedQueries.PostMonitoringProcessDetail, params={"data": eachLog, "hostname": hostname}))
 
 
-
-class QuerySerializer:   
+class QuerySerializer:
     @staticmethod
-    def getDesignatedQuery(reqType:SupportedQueries, params:dict) -> dict:
+    def getDesignatedQuery(reqType: SupportedQueries, params: dict) -> dict:
         url, data = reqType.url, reqType.func(**params)
-        print("here",url,data)
-        return {"url":url,"data":json.dumps(data)}
-            
+        print("here", url, data)
+        return {"url": url, "data": json.dumps(data)}
--- server/templates/static/js/visualization.js
+++ server/templates/static/js/visualization.js
@@ -28,29 +28,145 @@ async function plot_max_used_application_chart(from = Math.floor(Date.now() / 10
         }
     };
     new Chart(
-        document.getElementById('maxappuse'),
+        document.getElementById('appUsageChart'),
         config
     );
 
 
 };
 
+async function plot_network_activity(from = Math.floor(Date.now() / 1000) - 604800, to = Math.floor(Date.now() / 1000)) {
+    let data = await fetch(`http://localhost:8000/networkusage?from_time=${from}&to_time=${to}`)
+        .then((response) => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    response = data
+    let labels = []
+    let plotData = []
+    for (let i = 0; i < response.length; i++) {
+        labels.push(response[i].key)
+        plotData.push(response[i].doc_count)
+
+    }
+
+    console.log(labels, plotData)
+
+    const config3 = {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    data: plotData,
+
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Doughnut Chart'
+                }
+            }
+        },
+    };
+    new Chart(
+        document.getElementById('netwrokActivityChart'),
+        config3
+    );
+
+}
+
 
 async function plot_user_activity_chart(from = Math.floor(Date.now() / 1000) - 604800, to = Math.floor(Date.now() / 1000)) {
-    console.log("defined")
-    const labels = ['Red', 'Orange', 'Yellow', 'Green', 'Blue'];
+    let data = await fetch(`http://localhost:8000/useractivity?from_time=${from}&to_time=${to}`)
+        .then((response) => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    response = data
+
+    let labels = []
+    let plotData = []
+    for (let i = 0; i < response.length; i++) {
+        labels.push(response[i].key)
+        plotData.push(response[i].doc_count)
+
+    }
+    console.log(labels, plotData)
+
+    // const labels = ['Red', 'Orange', 'Yellow', 'Green', 'Blue'];
+
+    const config2 = {
+        type: 'polarArea',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'User Activity Logs',
+                    data: plotData,
+
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'User Activity Chart'
+                }
+            }
+        },
+    };
+    new Chart(
+        document.getElementById('userActivityChart'),
+        config2
+    );
+
+}
+
+
+
+async function plot_system_health_chart(from = Math.floor(Date.now() / 1000) - 604800, to = Math.floor(Date.now() / 1000)) {
+
     const data = {
-        labels: labels,
+        labels: ["temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1", "temperature", "cpu", "cpu2", "cpu1"],
         datasets: [
             {
                 label: 'Dataset 1',
-                data: [10, 20, 30, 4, 2],
+                data: [10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81, 10, 20, 30, 81]
 
+            },
+            {
+                label: 'Dataset 2',
+                data: [40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11, 40, 30, 12, 11]
             }
         ]
     };
-    const config2 = {
-        type: 'polarArea',
+
+    const config4 = {
+        type: 'line',
         data: data,
         options: {
             responsive: true,
@@ -60,14 +176,20 @@ async function plot_user_activity_chart(from = Math.floor(Date.now() / 1000) - 6
                 },
                 title: {
                     display: true,
-                    text: 'Chart.js Polar Area Chart'
+                    text: 'Chart.js Line Chart'
                 }
             }
         },
     };
     new Chart(
-        document.getElementById('myChart2'),
-        config2
+        document.getElementById('system_chart2'),
+        config4
     );
+    new Chart(
+        document.getElementById('system_chart'),
+        config4
+    );
+
+
 
 }
